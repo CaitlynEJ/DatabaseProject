def34c596ce2950f9b7091a609d8fbf0f5656c15
Caitlyn Jameson <59493115+CaitlynEJ@users.noreply.github.com>
2021-01-15 15:00:12 -0600

    Update dbcon.js
    removed personal information

--- superhero_school/dbcon.js
+++ superhero_school/dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
+  host            : '',
   user            : '',
   password        : '',
   database        : ''
